623da43ac47ec51aa064770b93d63e8cd47cc021
Sakurayo-Crispita <sagato31@outlook.com>
2026-03-21 02:02:04 -0500

    raro

--- utils/constants.py
+++ utils/constants.py
@@ -35,5 +35,11 @@ YDL_OPTIONS = {
     'source_address': '0.0.0.0',
     'noplaylist': True, 
     'cookiefile': 'cookies.txt',
-    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0'
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0',
+    'extractor_args': {
+        'youtube': {
+            'player_client': ['android', 'web'],
+            'skip': ['dash', 'hls']
+        }
+    }
 }
