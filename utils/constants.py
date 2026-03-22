93546a17a06a081607b1b5a04bf7eb93a9591613
Sakurayo-Crispita <sagato31@outlook.com>
2026-03-22 03:54:46 -0500

    Hardcoded Node path to /usr/bin/node and simplified clients

--- utils/constants.py
+++ utils/constants.py
@@ -37,10 +37,11 @@ YDL_OPTIONS = {
     'noplaylist': True, 
     'proxy': 'http://f3aadc356489963dafc5:060b354af4eb4134@gw.dataimpulse.com:823',
     'nocheckcertificate': True,
-    'extractor_args': {
         'youtube': {
             'player_client': ['web', 'mweb'],
+            'js_runtimes': ['/usr/bin/node']
         },
         'cookiefile': 'cookies.txt'
     }
 }
+
