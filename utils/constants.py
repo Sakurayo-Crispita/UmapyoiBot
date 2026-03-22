b79e952006d9ec23c441e9063ad5932d8595d28d
Sakurayo-Crispita <sagato31@outlook.com>
2026-03-22 03:48:23 -0500

    Simplified YDL_OPTIONS and removed js_runtimes for better resilience

--- utils/constants.py
+++ utils/constants.py
@@ -39,9 +39,8 @@ YDL_OPTIONS = {
     'nocheckcertificate': True,
     'extractor_args': {
         'youtube': {
-            'player_client': ['android_music', 'web'],
+            'player_client': ['web', 'mweb'],
         },
         'cookiefile': 'cookies.txt'
     }
 }
-
